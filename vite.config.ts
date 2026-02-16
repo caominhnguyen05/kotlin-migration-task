e2563d01c6228c12d73d62d9187465c6169eb4f4
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2026-02-17 00:04:27 +0100

    Add noExternal in SRR bundling

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    noExternal: [
+      "@rescui/button",
+      "@rescui/card",
+      "@rescui/checkbox",
+      "@rescui/colors",
+      "@rescui/icons",
+      "@rescui/input",
+      "@rescui/tab-list",
+      "@rescui/tooltip",
+      "@rescui/typography",
+      "@rescui/ui-contexts",
+      "@jetbrains/kotlin-web-site-ui",
+    ],
+  },
 });
