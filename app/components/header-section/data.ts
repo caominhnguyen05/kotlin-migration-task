09b7fc712e20245ddf8ddafab236906082d810a7
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2026-02-17 00:04:44 +0100

    Migrate header-section component

---
+++ app/components/header-section/data.ts
@@ -0,0 +1,45 @@
+import serverSideImg from "../images/good-for/server-side.svg";
+import mobileImg from "../images/good-for/mobile.svg";
+import androidImg from "../images/good-for/android.svg";
+import webImg from "../images/good-for/web.svg";
+
+// Define a new interface for card item type
+export interface CardItem {
+  id: number;
+  title: string;
+  subTitle: string;
+  link: string;
+  img: string;
+}
+
+export const cardsData: CardItem[] = [
+  {
+    id: 1,
+    title: "Multiplatform Mobile",
+    subTitle:
+      "Share the logic of your Android and iOS apps while keeping UX native",
+    link: "#",
+    img: mobileImg,
+  },
+  {
+    id: 2,
+    title: "Server-side",
+    subTitle: "Modern development experience with familiar JVM technology",
+    link: "#",
+    img: serverSideImg,
+  },
+  {
+    id: 3,
+    title: "Web Frontend",
+    subTitle: "Extend your projects to web",
+    link: "#",
+    img: webImg,
+  },
+  {
+    id: 4,
+    title: "Android",
+    subTitle: "Recommended by Google for building Android apps",
+    link: "#",
+    img: androidImg,
+  },
+];
